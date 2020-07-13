047370f27cc08dbaddfbfcd774f6076baec53ba9
roseline124 <guseod24@gmail.com>
2020-07-13 21:22:01 +0900

    Add sample schema, resolver

---
+++ src/resolverMap.ts
@@ -0,0 +1,10 @@
+// resolverMap.ts
+import { IResolvers } from "graphql-tools";
+const resolverMap: IResolvers = {
+  Query: {
+    helloWorld(_: void, args: void): string {
+      return `👋 Hello world! 👋`;
+    },
+  },
+};
+export default resolverMap;
