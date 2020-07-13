82b71a9b62d5b6602f92c9fcf89ad9dafd2a3ca5
roseline124 <guseod24@gmail.com>
2020-07-14 02:14:58 +0900

    Add db config

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,9 @@ import dotenv from 'dotenv'
 dotenv.config({ path: path.join(__dirname, '../.env') })
 
 // Add environment variables from .env
-export const MYSQL_DATABASE = process.env.MYSQL_DATABASE
-export const MYSQL_HOST = process.env.MYSQL_HOST
-export const MYSQL_PORT = process.env.MYSQL_PORT
-export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD
-export const MYSQL_USERNAME = process.env.MYSQL_USERNAME
+export const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'sb'
+export const MYSQL_HOST = process.env.MYSQL_HOST || '127.0.0.1'
+export const MYSQL_PORT = process.env.MYSQL_PORT || 9876
+export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || '1234'
+export const MYSQL_USERNAME = process.env.MYSQL_USERNAME || 'root'
+export const NODE_ENV = process.env.NODE_ENV || 'development'
