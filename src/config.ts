a1b18fdc863696e7e89e62df22702467fe5c4987
roseline124 <guseod24@gmail.com>
2020-07-14 02:25:44 +0900

    Update README.md (connect db)

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config({ path: path.join(__dirname, '../.env') })
 
 // Add environment variables from .env
-export const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'sb'
+export const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'studybuddies'
 export const MYSQL_HOST = process.env.MYSQL_HOST || '127.0.0.1'
 export const MYSQL_PORT = process.env.MYSQL_PORT || 9876
 export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || '1234'
