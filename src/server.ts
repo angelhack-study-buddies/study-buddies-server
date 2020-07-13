61fe4883120707206e493f7d3f6ebe7caf4c5427
roseline124 <guseod24@gmail.com>
2020-07-13 21:42:43 +0900

    Apply lint

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import depthLimit from "graphql-depth-limit";
-import { createServer } from "http";
-import compression from "compression";
-import cors from "cors";
-import schema from "./schema";
-const app = express();
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import depthLimit from 'graphql-depth-limit'
+import { createServer } from 'http'
+import compression from 'compression'
+import cors from 'cors'
+import schema from './schema'
+const app = express()
 const server = new ApolloServer({
   schema,
   validationRules: [depthLimit(7)],
   playground: true,
-});
-app.use("*", cors());
-app.use(compression());
-server.applyMiddleware({ app, path: "/graphql" });
-const httpServer = createServer(app);
+})
+app.use('*', cors())
+app.use(compression())
+server.applyMiddleware({ app, path: '/graphql' })
+const httpServer = createServer(app)
 httpServer.listen({ port: 3000 }, (): void =>
-  console.log(`\n🔥GraphQL is running on http://localhost:3000/graphql`)
-);
+  console.log(`\n🔥GraphQL is running on http://localhost:3000/graphql`),
+)
