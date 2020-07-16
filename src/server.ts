bb96f0721f9c56f3fe5e8a6646c4c9202ced0c4e
roseline124 <guseod24@gmail.com>
2020-07-16 17:12:10 +0900

    Store session id

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import compression from 'compression'
 import cors from 'cors'
 import depthLimit from 'graphql-depth-limit'
 import express from 'express'
+import session from 'express-session'
+import MySQLStore from 'express-mysql-session'
 import passport from 'passport'
 import { ApolloServer } from 'apollo-server-express'
 import { createServer } from 'http'
@@ -9,6 +11,15 @@ import { createServer } from 'http'
 import schema from './schema'
 import { sequelize } from './models'
 import { passportInitialize } from './passport'
+import {
+  CLIENT_BASE_URL,
+  COOKIE_SECRET,
+  MYSQL_HOST,
+  MYSQL_USERNAME,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+  MYSQL_PORT,
+} from './config'
 
 async function run() {
   sequelize.sync()
@@ -25,18 +36,35 @@ async function run() {
 
   passportInitialize()
 
+  const MysqlSessionStore = new MySQLStore(session)
+  app.use(
+    session({
+      store: new MysqlSessionStore({
+        host: MYSQL_HOST,
+        user: MYSQL_USERNAME,
+        password: MYSQL_PASSWORD,
+        database: MYSQL_DATABASE,
+        port: Number(MYSQL_PORT),
+      }),
+      secret: COOKIE_SECRET,
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
+        domain: CLIENT_BASE_URL,
+        secure: true,
+      },
+    }),
+  )
   app.use(passport.initialize())
   app.use(passport.session())
 
-  app.get('/', (req, res) => {
-    res.json({ user: req.user })
-  })
   app.get('/login', passport.authenticate('google', { scope: ['profile', 'email'] }))
   app.get(
     '/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/login' }),
     (req, res) => {
-      res.redirect('/')
+      res.redirect(CLIENT_BASE_URL)
     },
   )
 
