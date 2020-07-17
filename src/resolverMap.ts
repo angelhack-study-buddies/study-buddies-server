3024bbd8f29761007d1af0e4ac7bd72380548697
chloe-codes1 <juhyun.kim@lindsey.edu>
2020-07-17 19:31:14 +0900

    Working on post api

--- src/resolverMap.ts
+++ src/resolverMap.ts
@@ -1,10 +1,22 @@
 // resolverMap.ts
 import { IResolvers } from 'graphql-tools'
+
+interface Post {
+  id: number
+  authorID: string
+  url: string
+  likeCount: number
+  createdAt: Date
+  updatedAt: Date
+  deletedAt: Date
+}
 const resolverMap: IResolvers = {
   Query: {
     helloWorld(_: void, args: void): string {
       return `👋 Hello world! 👋`
     },
+    posts: () => {},
   },
+  Mutations: {},
 }
 export default resolverMap
